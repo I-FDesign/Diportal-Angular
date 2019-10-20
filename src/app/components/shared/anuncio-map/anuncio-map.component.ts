254ffafc5c1b6d471681996ab79dc61f40a0d44a
I-FDesign <ifdesing.contacto@gmail.com>
2019-10-20 18:56:19 -0300

    Map marker was not showing on prod

--- src/app/components/shared/anuncio-map/anuncio-map.component.ts
+++ src/app/components/shared/anuncio-map/anuncio-map.component.ts
@@ -21,6 +21,7 @@ export class AnuncioMapComponent implements OnInit {
   map: any;
   marker: any;
 
+
   constructor(
     private geocoderService: GeocoderService,
     private anuncioService: AnuncioService
@@ -34,6 +35,21 @@ export class AnuncioMapComponent implements OnInit {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
+
+    const iconRetinaUrl = 'assets/leaflet/images/marker-icon-2x.png';
+    const iconUrl = 'assets/leaflet/images/marker-icon.png';
+    const shadowUrl = 'assets/leaflet/images/marker-shadow.png';
+    const iconDefault = L.icon({
+      iconRetinaUrl,
+      iconUrl,
+      shadowUrl,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      tooltipAnchor: [16, -28],
+      shadowSize: [41, 41]
+    });
+    L.Marker.prototype.options.icon = iconDefault;
 
     this.map.on('click', (e) => {
 
